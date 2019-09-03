7920e4e33df0bbac7eb380fbba82fe9d5bc4ccd0
Jun Luo <junluo2@illinois.edu>
2019-09-03 14:48:22 -0500

    Convert tabs into spaces in two go files

--- tcp-server-client-go/client/client.go
+++ tcp-server-client-go/client/client.go
@@ -10,87 +10,87 @@ particularly idiomatic, or well-written for that matter.
 package main
 
 import (
-	"bufio"
-	"flag"
-	"fmt"
-	"net"
-	"os"
-	"regexp"
-	"strconv"
-	"time"
+    "bufio"
+    "flag"
+    "fmt"
+    "net"
+    "os"
+    "regexp"
+    "strconv"
+    "time"
 )
 
 var host = flag.String("host", "localhost", "The hostname or IP to connect to; defaults to \"localhost\".")
 var port = flag.Int("port", 8000, "The port to connect to; defaults to 8000.")
 
 func main() {
-	flag.Parse()
+    flag.Parse()
 
-	dest := *host + ":" + strconv.Itoa(*port)
-	fmt.Printf("Connecting to %s...\n", dest)
+    dest := *host + ":" + strconv.Itoa(*port)
+    fmt.Printf("Connecting to %s...\n", dest)
 
-	conn, err := net.Dial("tcp", dest)
+    conn, err := net.Dial("tcp", dest)
 
-	if err != nil {
-		if _, t := err.(*net.OpError); t {
-			fmt.Println("Some problem connecting.")
-		} else {
-			fmt.Println("Unknown error: " + err.Error())
-		}
-		os.Exit(1)
-	}
+    if err != nil {
+        if _, t := err.(*net.OpError); t {
+            fmt.Println("Some problem connecting.")
+        } else {
+            fmt.Println("Unknown error: " + err.Error())
+        }
+        os.Exit(1)
+    }
 
-	go readConnection(conn)
+    go readConnection(conn)
 
-	for {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("> ")
-		text, _ := reader.ReadString('\n')
+    for {
+        reader := bufio.NewReader(os.Stdin)
+        fmt.Print("> ")
+        text, _ := reader.ReadString('\n')
 
-		conn.SetWriteDeadline(time.Now().Add(1 * time.Second))
-		_, err := conn.Write([]byte(text))
-		if err != nil {
-			fmt.Println("Error writing to stream.")
-			break
-		}
-	}
+        conn.SetWriteDeadline(time.Now().Add(1 * time.Second))
+        _, err := conn.Write([]byte(text))
+        if err != nil {
+            fmt.Println("Error writing to stream.")
+            break
+        }
+    }
 }
 
 func readConnection(conn net.Conn) {
-	for {
-		scanner := bufio.NewScanner(conn)
+    for {
+        scanner := bufio.NewScanner(conn)
 
-		for {
-			ok := scanner.Scan()
-			text := scanner.Text()
+        for {
+            ok := scanner.Scan()
+            text := scanner.Text()
 
-			command := handleCommands(text)
-			if !command {
-				fmt.Printf("\b\b** %s\n> ", text)
-			}
+            command := handleCommands(text)
+            if !command {
+                fmt.Printf("\b\b** %s\n> ", text)
+            }
 
-			if !ok {
-				fmt.Println("Reached EOF on server connection.")
-				break
-			}
-		}
-	}
+            if !ok {
+                fmt.Println("Reached EOF on server connection.")
+                break
+            }
+        }
+    }
 }
 
 func handleCommands(text string) bool {
-	r, err := regexp.Compile("^%.*%$")
-	if err == nil {
-		if r.MatchString(text) {
+    r, err := regexp.Compile("^%.*%$")
+    if err == nil {
+        if r.MatchString(text) {
 
-			switch {
-			case text == "%quit%":
-				fmt.Println("\b\bServer is leaving. Hanging up.")
-				os.Exit(0)
-			}
+            switch {
+            case text == "%quit%":
+                fmt.Println("\b\bServer is leaving. Hanging up.")
+                os.Exit(0)
+            }
 
-			return true
-		}
-	}
+            return true
+        }
+    }
 
-	return false
+    return false
 }
