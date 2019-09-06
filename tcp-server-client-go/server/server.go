2c93b6823b753c7c86cc5759075465daa6a201c7
Jun Luo <junluo2@illinois.edu>
2019-09-06 10:32:48 -0500

    Revert "Convert tabs into spaces in two go files"
    Golang uses tabs for indentation
    
    This reverts commit 7920e4e33df0bbac7eb380fbba82fe9d5bc4ccd0.

--- tcp-server-client-go/server/server.go
+++ tcp-server-client-go/server/server.go
@@ -10,77 +10,77 @@ particularly idiomatic, or well-written for that matter.
 package main
 
 import (
-    "bufio"
-    "flag"
-    "fmt"
-    "net"
-    "os"
-    "strconv"
-    "time"
+	"bufio"
+	"flag"
+	"fmt"
+	"net"
+	"os"
+	"strconv"
+	"time"
 )
 
 var addr = flag.String("addr", "", "The address to listen to; default is \"\" (all interfaces).")
 var port = flag.Int("port", 8000, "The port to listen on; default is 8000.")
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    fmt.Println("Starting server...")
+	fmt.Println("Starting server...")
 
-    src := *addr + ":" + strconv.Itoa(*port)
-    listener, _ := net.Listen("tcp", src)
-    fmt.Printf("Listening on %s.\n", src)
+	src := *addr + ":" + strconv.Itoa(*port)
+	listener, _ := net.Listen("tcp", src)
+	fmt.Printf("Listening on %s.\n", src)
 
-    defer listener.Close()
+	defer listener.Close()
 
-    for {
-        conn, err := listener.Accept()
-        if err != nil {
-            fmt.Printf("Some connection error: %s\n", err)
-        }
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Printf("Some connection error: %s\n", err)
+		}
 
-        go handleConnection(conn)
-    }
+		go handleConnection(conn)
+	}
 }
 
 func handleConnection(conn net.Conn) {
-    remoteAddr := conn.RemoteAddr().String()
-    fmt.Println("Client connected from " + remoteAddr)
+	remoteAddr := conn.RemoteAddr().String()
+	fmt.Println("Client connected from " + remoteAddr)
 
-    scanner := bufio.NewScanner(conn)
+	scanner := bufio.NewScanner(conn)
 
-    for {
-        ok := scanner.Scan()
+	for {
+		ok := scanner.Scan()
 
-        if !ok {
-            break
-        }
+		if !ok {
+			break
+		}
 
-        handleMessage(scanner.Text(), conn)
-    }
+		handleMessage(scanner.Text(), conn)
+	}
 
-    fmt.Println("Client at " + remoteAddr + " disconnected.")
+	fmt.Println("Client at " + remoteAddr + " disconnected.")
 }
 
 func handleMessage(message string, conn net.Conn) {
-    fmt.Println("> " + message)
+	fmt.Println("> " + message)
 
-    if len(message) > 0 && message[0] == '/' {
-        switch {
-        case message == "/time":
-            resp := "It is " + time.Now().String() + "\n"
-            fmt.Print("< " + resp)
-            conn.Write([]byte(resp))
+	if len(message) > 0 && message[0] == '/' {
+		switch {
+		case message == "/time":
+			resp := "It is " + time.Now().String() + "\n"
+			fmt.Print("< " + resp)
+			conn.Write([]byte(resp))
 
-        case message == "/quit":
-            fmt.Println("Quitting.")
-            conn.Write([]byte("I'm shutting down now.\n"))
-            fmt.Println("< " + "%quit%")
-            conn.Write([]byte("%quit%\n"))
-            os.Exit(0)
+		case message == "/quit":
+			fmt.Println("Quitting.")
+			conn.Write([]byte("I'm shutting down now.\n"))
+			fmt.Println("< " + "%quit%")
+			conn.Write([]byte("%quit%\n"))
+			os.Exit(0)
 
-        default:
-            conn.Write([]byte("Unrecognized command.\n"))
-        }
-    }
+		default:
+			conn.Write([]byte("Unrecognized command.\n"))
+		}
+	}
 }
