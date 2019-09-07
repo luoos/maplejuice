078d6ff094e2b4b3511e91e745957ec2d0813667
Shen <rs20@illinois.edu>
2019-09-07 17:21:45 -0500

    added scripts for start and kill

--- scripts/start_all_server.sh
+++ scripts/start_all_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-~/distributed_log_querier/build.sh
-for h in `cat ~/distributed_log_querier/scripts/hosts`;
-do scp ~/distributed_log_querier/server $h:~; done
-#pssh -h hosts -i "nohup ~/server &"
+~/distributed_log_querier/scripts/build.sh
+for h in `cat ~/distributed_log_querier/scripts/servers`;
+do scp ~/distributed_log_querier/server $h:~ && sleep 1; done
+pssh -h ~/distributed_log_querier/scripts/servers -i "nohup ~/server </dev/null &>/dev/null &"
