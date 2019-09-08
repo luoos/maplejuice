873cb9686cae6684963ef755536aa260e6230da2
Shen <rs20@illinois.edu>
2019-09-08 13:35:40 -0500

    changed client.go to work in for loop situation

--- src/client/client.go
+++ src/client/client.go
@@ -9,21 +9,35 @@ import (
 	"os"
 	. "result"
 	"strconv"
+	"bufio"
 )
 
-var host = flag.String("host", "localhost", "The hostname or IP to connect to; defaults to \"localhost\".")
 var port = flag.Int("port", 8000, "The port to connect to; defaults to 8000.")
-
+var servers_file = "./scripts/servers"
 func main() {
 	flag.Parse()
 	reg := os.Args[1]
+	file, err := os.Open(servers_file)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer file.Close()
 
-	dest := *host + ":" + strconv.Itoa(*port)
+	scanner := bufio.NewScanner(file) 
+	scanner.Split(bufio.ScanLines)
+	for scanner.Scan() {
+		host := scanner.Text()
+		dest := host + ":" + strconv.Itoa(*port)
+		request_grep(dest, reg)
+	}
+}
+func request_grep(dest string, reg string) {
 	conn, err := rpc.Dial("tcp", dest)
 
 	if err != nil {
 		if _, t := err.(*net.OpError); t {
-			fmt.Println("Some problem connecting.")
+			fmt.Println("Some problem connecting.", err)
 		} else {
 			fmt.Println("Unknown error: " + err.Error())
 		}
