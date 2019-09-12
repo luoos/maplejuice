47bbb8d88a3a20e5739a5c3658d34662b9145c85
Jun Luo <junluo2@illinois.edu>
2019-09-12 15:09:54 -0500

    Add scripts to run and stop servers

--- scripts/run_servers.sh
+++ scripts/run_servers.sh
@@ -1,1 +1,3 @@
-pssh -h ./scripts/servers_luo -i "nohup /usr/app/log_querier/server </dev/null &>/dev/null &"
+PORT=${1:-8000} # default port 8000
+
+pssh -h ./scripts/servers_luo -i "nohup /usr/app/log_querier/server --port=$PORT </dev/null &>/dev/null &"
