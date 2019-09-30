e36d118bd8dbc347757261344d8909a2690b0ae7
Jun Luo <junluo2@illinois.edu>
2019-09-30 13:14:06 -0500

    Add GetNextKNodes for MemberList and fix a bug

--- src/memberlist/memberlist.go
+++ src/memberlist/memberlist.go
@@ -3,28 +3,28 @@ package memberlist
 import "log"
 
 type MemberNode struct {
-	id          int
-	heartbeat_t int
-	ip          string
-	port        string
-	prev        *MemberNode
-	next        *MemberNode
+	Id          int
+	Heartbeat_t int
+	Ip          string
+	Port        string
+	Prev        *MemberNode
+	Next        *MemberNode
 }
 
 func CreateNode(id int, ip, port string, heartbeat_t int) *MemberNode {
-	new_node := &MemberNode{id: id, ip: ip, port: port, heartbeat_t: heartbeat_t}
-	new_node.prev = new_node
-	new_node.next = new_node
+	new_node := &MemberNode{Id: id, Ip: ip, Port: port, Heartbeat_t: heartbeat_t}
+	new_node.Prev = new_node
+	new_node.Next = new_node
 	return new_node
 }
 
 type MemberList struct {
-	Member_map map[int]MemberNode
+	Member_map map[int]*MemberNode
 	Size       int
 }
 
 func CreateMemberList(size int) *MemberList {
-	member_map := make(map[int]MemberNode)
+	member_map := make(map[int]*MemberNode)
 	memberList := &MemberList{Member_map: member_map, Size: size}
 	return memberList
 }
@@ -34,13 +34,13 @@ func (mbList *MemberList) InsertNode(id int, ip, port string, heartbeat_t int) {
 		log.Fatal("id exists")
 	}
 	new_node := CreateNode(id, ip, port, heartbeat_t)
-	mbList.Member_map[id] = *new_node
+	mbList.Member_map[id] = new_node
 	if len(mbList.Member_map) == 1 {
 		// No other node exists
 		return
 	}
 	cur := (id + 1) % mbList.Size
-	var next_node MemberNode
+	var next_node *MemberNode
 	for cur != id {
 		if _, ok := mbList.Member_map[cur]; ok {
 			next_node = mbList.Member_map[cur]
@@ -48,11 +48,11 @@ func (mbList *MemberList) InsertNode(id int, ip, port string, heartbeat_t int) {
 		}
 		cur = (cur + 1) % mbList.Size
 	}
-	pre_node := next_node.prev
-	pre_node.next = new_node
-	new_node.prev = pre_node
-	new_node.next = &next_node
-	next_node.prev = new_node
+	pre_node := next_node.Prev
+	pre_node.Next = new_node
+	new_node.Prev = pre_node
+	new_node.Next = next_node
+	next_node.Prev = new_node
 }
 
 func (mbList *MemberList) FindLeastFreeId() int {
@@ -73,9 +73,23 @@ func (mbList *MemberList) DeleteNode(id int) {
 		return
 	}
 	cur_node := mbList.Member_map[id]
-	prev := cur_node.prev
-	next := cur_node.next
-	prev.next = next
-	next.prev = prev
+	prev := cur_node.Prev
+	next := cur_node.Next
+	prev.Next = next
+	next.Prev = prev
 	delete(mbList.Member_map, id)
+}
+
+func (mbList MemberList) GetNextKNodes(id, k int) []MemberNode {
+	if _, exist := mbList.Member_map[id]; !exist {
+		log.Panic("start id doesn't exit in memberlist")
+		return nil
+	}
+	arr := make([]MemberNode, 0)
+	next := mbList.Member_map[id].Next
+	for i := 0; i < k && next.Id != id; i++ {
+		arr = append(arr, *next)
+		next = next.Next
+	}
+	return arr
 }
--- src/test/memberlist_test.go
+++ src/test/memberlist_test.go
@@ -65,3 +65,50 @@ func TestFindFreeId(t *testing.T) {
 		t.Fatalf("incorrect least free id: %d", id)
 	}
 }
+
+func TestGetNextKNodes(t *testing.T) {
+	size := 10
+	mbList := memberlist.CreateMemberList(size)
+	mbList.InsertNode(0, "0.0.0.0", "90", 1)
+	mbList.InsertNode(2, "0.0.0.0", "90", 1)
+	mbList.InsertNode(4, "0.0.0.0", "90", 1)
+	mbList.InsertNode(5, "0.0.0.0", "90", 1)
+	arr := mbList.GetNextKNodes(0, 3)
+	if len(arr) != 3 {
+		t.Fatalf("length not match, expect %d, got %d", 3, len(arr))
+	}
+	if arr[0].Id != 2 {
+		t.Fatal("incorrect next node")
+	}
+	if arr[1].Id != 4 {
+		t.Fatal("incorrect next node")
+	}
+	if arr[2].Id != 5 {
+		t.Fatal("incorrect next node")
+	}
+	mbList.DeleteNode(4)
+	arr = mbList.GetNextKNodes(0, 3)
+	if len(arr) != 2 {
+		t.Fatal("length not match")
+	}
+	if arr[0].Id != 2 {
+		t.Fatal("incorrect next node")
+	}
+	if arr[1].Id != 5 {
+		t.Fatal("incorrect next node")
+	}
+	if arr[1].Next.Id != 0 {
+		t.Fatal("incorrect next node")
+	}
+}
+
+func TestNodePointer(t *testing.T) {
+	size := 10
+	mbList := memberlist.CreateMemberList(size)
+	mbList.InsertNode(0, "0.0.0.0", "90", 1)
+	mbList.InsertNode(2, "0.0.0.0", "90", 1)
+	node0 := mbList.Member_map[0]
+	if node0.Next.Id != 2 {
+		t.Fatal("wrong next node")
+	}
+}
