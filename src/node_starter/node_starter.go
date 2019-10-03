a2d8f57d82db94440859416a5376e14cdb7e76e3
Jun Luo <junluo2@illinois.edu>
2019-10-03 00:29:03 -0500

    Update server addresses

--- src/node_starter/node_starter.go
+++ src/node_starter/node_starter.go
@@ -13,16 +13,16 @@ const (
 )
 
 var SERVER_LIST = []string{
-	"fa19-cs425-g17-01.cs.illinois.edu",
-	"fa19-cs425-g17-02.cs.illinois.edu",
-	"fa19-cs425-g17-03.cs.illinois.edu",
-	"fa19-cs425-g17-04.cs.illinois.edu",
-	"fa19-cs425-g17-05.cs.illinois.edu",
-	"fa19-cs425-g17-06.cs.illinois.edu",
-	"fa19-cs425-g17-07.cs.illinois.edu",
-	"fa19-cs425-g17-08.cs.illinois.edu",
-	"fa19-cs425-g17-09.cs.illinois.edu",
-	"fa19-cs425-g17-10.cs.illinois.edu",
+	"fa19-cs425-g17-01.cs.illinois.edu:" + PORT,
+	"fa19-cs425-g17-02.cs.illinois.edu:" + PORT,
+	"fa19-cs425-g17-03.cs.illinois.edu:" + PORT,
+	"fa19-cs425-g17-04.cs.illinois.edu:" + PORT,
+	"fa19-cs425-g17-05.cs.illinois.edu:" + PORT,
+	"fa19-cs425-g17-06.cs.illinois.edu:" + PORT,
+	"fa19-cs425-g17-07.cs.illinois.edu:" + PORT,
+	"fa19-cs425-g17-08.cs.illinois.edu:" + PORT,
+	"fa19-cs425-g17-09.cs.illinois.edu:" + PORT,
+	"fa19-cs425-g17-10.cs.illinois.edu:" + PORT,
 }
 
 func main() {
