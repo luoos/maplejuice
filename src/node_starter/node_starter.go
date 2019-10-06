5b39d0327ca9bcd0200902bd14fa7c745da34d02
Jun Luo <junluo2@illinois.edu>
2019-10-05 21:03:08 -0500

    No need to wait after calling joining

--- src/node_starter/node_starter.go
+++ src/node_starter/node_starter.go
@@ -6,7 +6,6 @@ import (
 	"os/signal"
 	. "slogger"
 	"syscall"
-	"time"
 )
 
 const (
@@ -37,10 +36,6 @@ func main() {
 		node.Join(add)
 	} else {
 		node.InitMemberList()
-	}
-	for node.MbList == nil {
-		time.Sleep(1 * time.Second)
-		SLOG.Print("waiting for setup...")
 	}
 	go node.SendHeartbeatRoutine()
 	go node.CheckFailureRoutine()
