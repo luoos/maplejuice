e08289151b74ab66e40f8987a969e71bf0119ebe
Ruochen Shen <rs20@illinois.edu>
2019-11-02 15:57:29 -0500

    Change script

--- scripts/delete_node_logs.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-pssh -h "scripts/servers" -i "rm /apps/logs/node.log"
--- scripts/deploy_dnode.sh
+++ scripts/deploy_dnode.sh
@@ -5,6 +5,4 @@ for h in `cat ./scripts/servers`;
 do
     scp ./node_starter $h:/usr/app/dnode;
 done
-systemctl start dnode
-sleep 5
 ./scripts/start_all_nodes.sh
